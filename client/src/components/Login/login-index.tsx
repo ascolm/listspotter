3e51c72e5d7bcb8581f9afcefad9724dc971f00d
ascolm <mertascioglu@gmail.com>
2021-03-03 18:45:56 +0100

    feat(login): setup login to spotify authorize routing

--- client/src/components/Login/login-index.tsx
+++ client/src/components/Login/login-index.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react';
 // import { useState, useEffect } from 'react';
 
-export interface Props {
+// TODO: MOVE OUT
+const baseUrl = 'https://accounts.spotify.com/authorize';
 
+interface QueryParams {
+  client_id: string,
+  response_type: string,
+  redirect_uri: string,
+  state: string
+}
+
+const queryParams: QueryParams = {
+  client_id: 'decae5e905644494a2de08b59116f140',
+  response_type: 'code',
+  redirect_uri: 'http://localhost:3000/main',
+  state: 'bayburt'
+}
+
+function buildQueryUrl (url: string, params: QueryParams) : string {
+  return `${baseUrl}?client_id=${params.client_id}&response_type=${params.response_type}&redirect_uri=${params.redirect_uri}&state=${params.state}`;
+}
+// /TODO
+
+
+export interface Props {
 }
 
 const Login: React.FC<Props> = () => {
   return (
-    <h1>This is the login page canım.</h1>
+    <div>
+      <h1>This is the login page canım.</h1>
+      <a href={buildQueryUrl(baseUrl, queryParams)}>LOGIN</a>
+    </div>
    );
 }
 
