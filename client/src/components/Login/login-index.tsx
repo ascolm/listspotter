43d0777b58180f9db61689f21e7d5c9e65629565
ascolm <mertascioglu@gmail.com>
2021-03-03 18:07:02 +0100

    feat(app): setup router & create login, main components

---
+++ client/src/components/Login/login-index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+// import { useState, useEffect } from 'react';
+
+export interface Props {
+
+}
+
+const Login: React.FC<Props> = () => {
+  return (
+    <h1>This is the login page canım.</h1>
+   );
+}
+
+export default Login;
---
+++ client/src/components/Main/main-index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+// import { useState, useEffect } from 'react';
+
+export interface Props {
+
+}
+
+const Main: React.FC<Props> = () => {
+  return (
+    <h1>Main'e hoşgeldiniz aq.</h1>
+  );
+}
+
+export default Main;
