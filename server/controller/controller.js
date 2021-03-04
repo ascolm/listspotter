52051b116703ed4d2bbd29481a7e09f4da7a75ee
ascolm <mertascioglu@gmail.com>
2021-03-04 13:25:24 +0100

    refactor(controller): refactor controller & gettoken.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,58 +1,33 @@
-const { clientID, clientSecret } = require('../config');
 const axios = require('axios');
-
-const spotifyTokenUrl = 'https://accounts.spotify.com/api/token';
+const getTokens = require('./getTokens');
 const spotifyTracksUrl = 'https://api.spotify.com/v1/me/tracks';
-const redirectUri = 'http://localhost:3000/main';
 const tokenValidForMs = 1000 * 60 * 45 // This is equal to 45 mins (Token is valid for 1 hour)
+
+
 let tokens;
 
-async function getTokens (code) {
-  let tokenResponse;
-  try {
-      tokenResponse = await axios.request({
-      method: 'POST',
-      url: spotifyTokenUrl,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&client_id=${clientID}&client_secret=${clientSecret}`
-    });
-  } catch (err) {
-  }
+// TODO: CHECK ERROR HANDLING IN CATCH METHODS // CREATE CUSTOM HANDLER MIDDLEWARE
 
-  tokens = tokenResponse.data;
-  setTimeout(() => tokens = '', tokenValidForMs);
-}
-
-// TODO: REFACTOR
-exports.getTracks = async (req, res) => {
+exports.getTracks = async (req, res, next) => {
   const {code, offset} = req.body;
 
   if (!tokens) {
-    try {
-      await getTokens(code);
-    } catch (err) {
-      console.log('🔥Error while getting tokens from Spotify');
-      console.log(err);
-      res.statusCode = 500;
-      res.send('Spotify authentication failed.');
-    }
+    tokens = await getTokens(code, next);
+    setTimeout(() => tokens = '', tokenValidForMs);
   }
 
-  try {
-    const trackResponse = await axios.request({
-      method: 'GET',
-      url: spotifyTracksUrl + `?offset=${offset}&limit=50`,
-      headers: {
-        'Authorization': `Bearer ${tokens['access_token']}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-    });
-
+  axios.request({
+    method: 'GET',
+    url: spotifyTracksUrl + `?offset=${offset}&limit=50`,
+    headers: {
+      'Authorization': `Bearer ${tokens['access_token']}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+  })
+  .then((trackResponse) => {
+    console.log(trackResponse.data.href);
     res.statusCode = 200;
     res.send(trackResponse.data);
-  } catch (err) {
-    console.log(err);
-  }
+  })
+  .catch(next);
 }
---
+++ server/controller/getTokens.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const { clientID, clientSecret } = require('../config');
+const spotifyTokenUrl = 'https://accounts.spotify.com/api/token';
+const redirectUri = 'http://localhost:3000/main';
+
+module.exports = async (code, next) => {
+  return axios.request({
+    method: 'POST',
+    url: spotifyTokenUrl,
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&client_id=${clientID}&client_secret=${clientSecret}`
+  })
+  .then((tokenResponse) => {
+    return tokenResponse.data;
+  })
+  .catch(next);
+}
