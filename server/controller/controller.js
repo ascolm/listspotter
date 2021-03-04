f65594b106e0c18fa2bac67370917e70cef9c191
ascolm <mertascioglu@gmail.com>
2021-03-04 11:12:03 +0100

    feat(gettrack): setup chained calls to Spotify to get full list of songs, load update state on cli

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -4,35 +4,40 @@ const axios = require('axios');
 const spotifyTokenUrl = 'https://accounts.spotify.com/api/token';
 const spotifyTracksUrl = 'https://api.spotify.com/v1/me/tracks';
 const redirectUri = 'http://localhost:3000/main';
+const tokenValidForMs = 1000 * 60 * 45 // This is equal to 45 mins (Token is valid for 1 hour)
+let tokens;
 
-function getToken () {
-
-
-}
-
+// TODO: REFACTOR
 async function getTracks (req, res) {
-  const code = req.body.code;
+  const {code, offset} = req.body;
+
   try {
-    const tokenResponse = await axios.request({
-      method: 'POST',
-      url: spotifyTokenUrl,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&client_id=${clientID}&client_secret=${clientSecret}`
-    });
-    const tokens = tokenResponse.data;
+    if (!tokens) {
+      const tokenResponse = await axios.request({
+        method: 'POST',
+        url: spotifyTokenUrl,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&client_id=${clientID}&client_secret=${clientSecret}`
+      });
+
+      tokens = tokenResponse.data;
+      setTimeout(() => tokens = '', tokenValidForMs);
+    }
+
     const trackResponse = await axios.request({
       method: 'GET',
-      url: spotifyTracksUrl,
+      url: spotifyTracksUrl + `?offset=${offset}&limit=50`,
       headers: {
-        'Authorization': `Bearer ${tokens['access_token']}`
-      }
+        'Authorization': `Bearer ${tokens['access_token']}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
     });
-    console.log(trackResponse.data);
-    console.log(trackResponse.data.items[0]);
+
+    console.log(trackResponse.data.href);
     res.statusCode = 200;
-    res.send(trackResponse.data.items);
+    res.send(trackResponse.data);
   } catch (err) {
     console.log(err);
   }
