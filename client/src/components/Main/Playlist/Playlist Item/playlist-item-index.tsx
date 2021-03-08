e049c6afe662315f3acebeafb9aa18d8486d5833
ascolm <mertascioglu@gmail.com>
2021-03-08 19:31:31 +0100

    feat(playlist): concatenate artist names, text overflow in table fields switched to scrollable

--- client/src/components/Main/Playlist/Playlist Item/playlist-item-index.tsx
+++ client/src/components/Main/Playlist/Playlist Item/playlist-item-index.tsx
@@ -1,6 +1,6 @@
 import './playlist-item-style.scss';
 import { TrackItem } from 'interfaces/spotifyObjects';
-import { convertMsToMinsAndSecs } from './playlist-item-helpers';
+import { convertMsToMinsAndSecs, joinArtistNames } from './playlist-item-helpers';
 import moment from 'moment';
 
 
@@ -15,7 +15,7 @@ const PlaylistItem: React.FC<Props> = ({ track, disabled, toggleHandler }) => {
   return (
       <tr className={'playlist-item-container' + (disabled ? ' user-disabled':'')} onClick = {() => toggleHandler(track.track.id)}>
         <td>{track.track.name}</td>
-        <td>{track.track.artists[0].name}</td>
+        <td>{joinArtistNames(track)}</td>
         <td>{track.track.album.name}</td>
         <td>{moment(track.added_at).fromNow()}</td>
         <td>{convertMsToMinsAndSecs(track.track.duration_ms)}</td>
