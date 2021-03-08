cc63051823f2f27c1fbba57cc8486980bc0bfb5d
ascolm <mertascioglu@gmail.com>
2021-03-08 15:57:27 +0100

    feat(genres): create loading message for genres

--- client/src/components/Main/Genres/genres-index.tsx
+++ client/src/components/Main/Genres/genres-index.tsx
@@ -20,15 +20,22 @@ const Genres: React.FC<Props> = ({genreList, artists, selectHandler, loaded}) =>
   return (
 
     <div className={'genres-container' + (loaded ? ' loaded':'')}>
-      <SearchBar placeholder='Search genres' searchValue={searchValue} setSearchValue={setSearchValue}/>
+      {loaded ?
+        <>
+          <SearchBar placeholder='Search genres' searchValue={searchValue} setSearchValue={setSearchValue}/>
+
+          {getSortedGenreKeys(searchValue ? getMatchingGenres(searchValue, genreList) : genreList, artists.filter((artist) => artist.selected)).map((genreName) => {
+              return <GenreItem key={genreName} genreName={genreName} artistCount={countUnselected(genreList, genreName, artists)} selectHandler={selectHandler} selected={genreList[genreName].selected}/>
+            })}
+        </>
+        :
+        <p className='loading-genres-message'>Loading your genres...</p>
+      }
       {/* - Genres will be sorted based on how many artists they contain (descending). Selected genres are always on top (getSortedGenreKeys)
       - If there is a search value entered in the searchbar, filter only genres whose names match with the search value       (getMatchingGenres)
       - For each genre, the artist count shows only the number of artists in that genre that the user has not selected so far.
         For example, say "Rock" has 5 artists, and "Modern Rock" has 3 artists, which are also under "Rock". If the user selects "Modern Rock" first, those 3 artists will be on display. On the left hand side, "Rock" genre will now show an artist count of 2, because the other 3 has already been chosen with the "Modern Rock" tag. */}
 
-      {getSortedGenreKeys(searchValue ? getMatchingGenres(searchValue, genreList) : genreList, artists.filter((artist) => artist.selected)).map((genreName) => {
-          return <GenreItem key={genreName} genreName={genreName} artistCount={countUnselected(genreList, genreName, artists)} selectHandler={selectHandler} selected={genreList[genreName].selected}/>
-        })}
     </div>
   );
 }
