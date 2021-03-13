eec99c9783eabaef345a6c2df25e7b0308aba8c7
Pamela <pmelacodes@gmail.com>
2021-03-13 15:11:13 +0000

    feat(genre_test): added for genre item

--- client/src/components/Main/Genres/Genre Item/__tests__/test.js
+++ client/src/components/Main/Genres/Genre Item/__tests__/test.js
@@ -7,10 +7,23 @@ describe('Genre Item', () => {
   it('appears on screen', () => {
     render(<GenreItem />);
   });
+
   it('renders the right name', () => {
     const genreName = 'metal';
     render(<GenreItem genreName={genreName} />);
     const name = screen.getByText('metal');
     expect(name).toBeInTheDocument();
+  });
+
+  it('renders the right number of artists', () => {
+    const artistCount = 3;
+    render(<GenreItem artistCount={artistCount} />);
+    const count = screen.getByText('+3 artists');
+    console.log(count);
+  });
+
+  it('only renders genre name when selected', () => {
+    const genreName = 'punk rock';
+    render(<GenreItem genreName={genreName} selected={true} />);
   });
 });
