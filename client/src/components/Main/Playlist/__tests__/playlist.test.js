3ee8f5fb9a6fa0f401749ac0cb62d688cf63cf9e
Gero <gero.a.kassing@gmail.com>
2021-03-15 12:51:04 +0000

    style(__tests__): renamed test files

---
+++ client/src/components/Main/Playlist/__tests__/playlist.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
+import Playlists from '../playlist-index';
+
+// describe('Playlist', () => {
+//   // should render on screen
+//   // it('', () => {})
+//   //
+//   // it('', () => {})
+//   // it('', () => {})
+// });
