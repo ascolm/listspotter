3c9c0d37a32717ceb66f675e6d22cb5ffefff81b
Gero <gero.a.kassing@gmail.com>
2021-03-16 12:03:33 +0000

    test(search-bar): searchbar component renders

--- client/src/components/common/Searchbar/__tests__/searchbar.test.js
+++ client/src/components/common/Searchbar/__tests__/searchbar.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import SearchBar from '../searchbar-index';
 
-describe('SearchBar', () => {});
+describe('SearchBar', () => {
+  it('should render the search-bar', () => {
+    const { container } = render(<SearchBar />);
+    const div = container.querySelector('div');
+    expect(div).toBeInTheDocument();
+  });
+});
