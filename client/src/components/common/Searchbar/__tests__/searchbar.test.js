b5e0abd73116d01f945609bb2307585f462e0696
Gero <gero.a.kassing@gmail.com>
2021-03-16 11:46:23 +0000

    test(searchbar): file created

---
+++ client/src/components/common/Searchbar/__tests__/searchbar.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
+import SearchBar from '../searchbar-index';
+
+describe('SearchBar', () => {});
