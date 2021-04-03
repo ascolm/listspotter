b31098faef8a0f9110db9ae8a2e292cffb51ad17
ascolm <mertascioglu@gmail.com>
2021-04-03 14:18:27 +0200

    refactor(login): separate redirect uri, client id into separate file

--- client/src/components/Login/login-queryParams.tsx
+++ client/src/components/Login/login-queryParams.tsx
@@ -1,24 +1,22 @@
 const baseUrl = 'https://accounts.spotify.com/authorize';
+const { clientId, redirectUri } = require('config');
 
 interface QueryParams {
   client_id: string,
   response_type: string,
   redirect_uri: string,
-  state: string,
   scope: string
 }
 
 const queryParams: QueryParams = {
-  client_id: 'decae5e905644494a2de08b59116f140',
+  client_id: clientId,
   response_type: 'code',
-  redirect_uri: 'http://localhost:3000/main',
-  state: 'bayburt',
-  // scope: 'user-library-read user-follow-modify'
+  redirect_uri: redirectUri,
   scope: 'user-library-read user-follow-read user-follow-modify playlist-modify-public'
 }
 
 function buildQueryUrl (url: string, params: QueryParams) : string {
-  return `${baseUrl}?client_id=${params.client_id}&response_type=${params.response_type}&redirect_uri=${params.redirect_uri}&scope=${encodeURIComponent(queryParams.scope)}&state=${params.state}`;
+  return `${baseUrl}?client_id=${params.client_id}&response_type=${params.response_type}&redirect_uri=${params.redirect_uri}&scope=${encodeURIComponent(queryParams.scope)}`;
 }
 
 const queryUrl = buildQueryUrl(baseUrl, queryParams);
