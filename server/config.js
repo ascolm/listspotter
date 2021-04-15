fa2d39c7edb5f34417b8265b2d047e6017b3a593
ascolm <mertascioglu@gmail.com>
2021-04-15 14:29:36 +0200

    build(config): add checks for prod & localhost to config files

--- server/config.js
+++ server/config.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+const inProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   serverHost: process.env.SERVER_HOST,
   serverPort: process.env.PORT || process.env.SERVER_PORT,
-  redirectUri: process.env.REDIRECT_URI,
+  redirectUri: inProduction ? process.env.REDIRECT_URI : process.env.REDIRECT_URI_LOCAL,
   spotifyTokenUrl: process.env.SPOTIFY_TOKEN_URL
 };
