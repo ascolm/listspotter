1a6137951ac8b9ec6f83974fe375d61a61ce57af
Mert Ascioglu <mertascioglu@gmail.com>
2021-10-01 14:51:45 +0200

    fix(lists): fix layout in playlist items and artist list

--- client/src/components/Main/Artists/artists-index.tsx
+++ client/src/components/Main/Artists/artists-index.tsx
@@ -12,9 +12,11 @@ const Artists: React.FC<Props> = ({ artistList, toggleHandler, loaded }) => {
   return (
     // SORT ARTISTS BY ALPHABETICAL ORDER
     <div className={'artists-container' + (loaded ? ' loaded':'')}>
-      {artistList.length === 0 && <p className="choose-genre-message">Choose a genre to view related artists.</p>}
-      {artistList.sort((a, b) => a.name < b.name ? -1 : 1)
-      .map((artist) => <ArtistItem artist={artist} key={artist.id} toggleHandler={toggleHandler}/>)}
+      <div className="artist-flex-wrapper">
+        {artistList.length === 0 && <p className="choose-genre-message">Choose a genre to view related artists.</p>}
+        {artistList.sort((a, b) => a.name < b.name ? -1 : 1)
+        .map((artist) => <ArtistItem artist={artist} key={artist.id} toggleHandler={toggleHandler}/>)}
+      </div>
     </div>
   );
 }
